21c5277edd6e2763692f20a3a7c299061de731ab
sabrinahafezi <sabrina@hafezi.ca>
2023-09-13 12:44:00 -0700

    Project 1

---
+++ VariablePractice/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main (String[] args) {
+
+        int x;
+        x = 113;
+        System.out.println("This is room # " + x);
+
+        double y;
+        y = 2.71828;
+        System.out.println("e is close to " + y);
+
+        String course = "Computer Science";
+        System.out.println("I am learning a bit about " + course);
+
+    }
+}
