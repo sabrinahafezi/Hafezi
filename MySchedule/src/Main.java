e93971aa92e98d211ce3409158bba045141d9445
sabrinahafezi <sabrina@hafezi.ca>
2023-09-13 12:47:35 -0700

    Project 2

---
+++ MySchedule/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String horizontalBorder = "+---------------------------------------------+";
+        System.out.println(horizontalBorder);
+
+        String rowOne = "| 1 |              Free block |        Myself |";
+        System.out.println(rowOne);
+
+        String rowTwo = "| 2 |              Physics 12 |      Mr. Tang |";
+        System.out.println(rowTwo);
+
+        String rowThree = "| 3 | Computer Programming 12 |   Mr. Zaremba |";
+        System.out.println(rowThree);
+
+        String rowFour = "| 4 |               French 12 | Mme. McCartin |";
+        System.out.println(rowFour);
+
+        String rowFive = "| 5 |           AP Biology 12 |    Mr. Wilkie |";
+        System.out.println(rowFive);
+
+        String rowSix = "| 6 |      English Studies 12 |   Ms. Silvers |";
+        System.out.println(rowSix);
+
+        String rowSeven = "| 7 |         Pre-Calculus 12 |      Ms. Reed |";
+        System.out.println(rowSeven);
+
+        String rowEight = "| 8 |             Calculus 12 |      Ms. Reed |";
+        System.out.println(rowEight);
+
+        System.out.println(horizontalBorder);
+
+    }
+}
